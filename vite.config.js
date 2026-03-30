deeaa93ceb9730617aadbb8b859523a467ff99c6
aaneleh <helenakurzgon@gmail.com>
2026-03-30 19:39:34 -0300

    deploy action

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -9,4 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: '/delivery-app/'
 })
